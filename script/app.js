0f338bd9065cc06f8caf7e155bed011f7e421941
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-05-04 10:58:48 -0400

    fix-web

---
+++ script/app.js
@@ -0,0 +1,12 @@
+const laguna = document.getElementById('abajo');
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 300) {
+        abajo.style.backgroundColor = '#04aa6b';
+        abajo.style.borderBottom = '3px solid #036';
+    } else {
+        abajo.style.backgroundColor = 'rgb(123, 157, 145)';
+          abajo.style.borderBottom = 'none';
+    }
+});
