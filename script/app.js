286eb59e74a8b570faee8f45feaf9949f40aa638
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-05-05 08:24:58 -0400

    fix-web

--- script/app.js
+++ script/app.js
@@ -4,7 +4,7 @@ document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
     if (scroll_position > 300) {
         abajo.style.backgroundColor = '#04aa6b';
-        abajo.style.borderBottom = '3px solid #036';
+        abajo.style.borderBottom = '3px solid #299684';
     } else {
         abajo.style.backgroundColor = 'rgb(123, 157, 145)';
           abajo.style.borderBottom = 'none';
